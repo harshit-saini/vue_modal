9c768aad374a7d37c3b63e06f21094014c73bfb5
harshit-saini <harshitr.x.t.10@gmail.com>
2020-03-13 22:56:09 +0530

    modal complete

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal-footer">
+    <h3>Modal Footer</h3>
+  </div>
+</template>
+
+<style  scoped>
+.modal-footer {
+  background: #428bca;
+  padding: 10px;
+  color: #fff;
+  text-align: center;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+</style>
+    
---
+++ components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="modal-header">
+    <span class="close" @click="closeModal">&times;</span>
+    <h2>Modal Header</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal: function() {
+      const modal = document.getElementById("my-modal");
+
+      document.querySelector(".close").addEventListener("click", () => {
+        modal.style.display = "none";
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal-header h2,
+.modal-footer h3 {
+  margin: 0;
+}
+
+.modal-header {
+  background: #428bca;
+  padding: 15px;
+  color: #fff;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+.close {
+  color: #ccc;
+  float: right;
+  font-size: 30px;
+  color: #fff;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
